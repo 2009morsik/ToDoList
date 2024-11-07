27b7791508dafa57d86587528812e0fdc3790b5e
morsik-pc <2009morsik@mail.ru>
2024-11-07 09:30:26 +0500

    first commit

--- app/index.tsx
+++ app/index.tsx
@@ -1,14 +1,41 @@
-import { Text, View } from "react-native";
+import React, {useState} from "react";
+import { Text, View, SafeAreaView, TouchableOpacity, StyleSheet } from "react-native";
+import {useRouter} from "expo-router";
+//import {Picker} from "@react-native-picker/picker"
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
+      <SafeAreaView style={styles.container}>
+          <View style={styles.heading}>
+          <Text style={styles.headingText}>
+              Список задач:
+          </Text>
+          </View>
+            <TouchableOpacity style={styles.addButton}>
+
+            </TouchableOpacity>
+      </SafeAreaView>
   );
 }
+const styles = StyleSheet.create({
+    container:{
+        flex: 1,
+        justifyContent: 'flex-start',
+        alignItems: 'center',
+        paddingTop: 60,
+        backgroundColor: "grey",
+    },
+    addButton:{
+        height: 50,
+        width: 50,
+        backgroundColor: "blue",
+        borderRadius: 50,
+    },
+    headingText:{
+        fontSize: 25,
+        fontWeight: "bold",
+    },
+    heading:{
+        flex:1,
+    }
+})
